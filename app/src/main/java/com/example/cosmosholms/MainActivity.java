af81d67a012328ad63ae057d74616396970934d1
Perseus-hub <alexander.soukharev@gmail.com>
2021-10-13 16:18:24 +0300

    Beta 0.0.1.

--- app/src/main/java/com/example/cosmosholms/MainActivity.java
+++ app/src/main/java/com/example/cosmosholms/MainActivity.java
@@ -3,13 +3,9 @@ package com.example.cosmosholms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    //Switch to window Activity when clicking on the picture
     public void change_act(View view) {
         Intent intent = new Intent(this, sort_by_age.class);
         startActivity(intent);
     }
 
+    //Switch to window Activity containing content WebView
     public void goToURL(View view) {
         Intent intent = new Intent(this, registeredActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/cosmosholms/registeredActivity.java
+++ app/src/main/java/com/example/cosmosholms/registeredActivity.java
@@ -2,10 +2,12 @@ package com.example.cosmosholms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -13,14 +15,23 @@ import android.webkit.WebViewClient;
 
 public class registeredActivity extends AppCompatActivity {
 
+    private WebView webContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered);
 
-        WebView webContent = (WebView) findViewById(R.id.webContent);
-        webContent.getSettings().getJavaScriptEnabled();
-        webContent.loadUrl("https://www.yandex.ru");
+        ShowWebViewWebContent();
+
+    }
+
+    //Going to the site at the specified URL and displaying its content in the element WebView
+    @SuppressLint("SetJavaScriptEnabled")
+    public void ShowWebViewWebContent(){
+        webContent = findViewById(R.id.webContent);
+        webContent.getSettings().setJavaScriptEnabled(true);
+        webContent.loadUrl("http://cosmosholms.com/welcome_page/");
 
         WebViewClient webViewClient = new WebViewClient() {
             @SuppressWarnings("deprecation") @Override
@@ -38,6 +49,17 @@ public class registeredActivity extends AppCompatActivity {
         webContent.setWebViewClient(webViewClient);
     }
 
+    //Clicking the Back button returns to the previous WebContent page
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode==KeyEvent.KEYCODE_BACK) && webContent.canGoBack()){
+            webContent.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    //Returns to the home page of the application
     public void back(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
